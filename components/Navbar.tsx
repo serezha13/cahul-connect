813a14186d54e36c80e874fb114a90ba1211d52c
Susan Anatolie <susan.anatol@gmail.com>
2023-09-10 02:29:53 +0300

    finished transport page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,29 @@
-import { createStyles, Header, Autocomplete, Group, Burger, rem } from '@mantine/core';
+import { Autocomplete, Group, Header, createStyles, rem } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { FaSearch, FaHotel } from 'react-icons/fa'
-import { MdOutlineHotel } from 'react-icons/md'
-import { BiCar, BiStore } from 'react-icons/bi'
-import { IoIosRestaurant } from 'react-icons/io'
-import { AiOutlineHome } from 'react-icons/ai'
 import Link from 'next/link';
-
+import { AiOutlineHome } from 'react-icons/ai';
+import { BiCar, BiStore } from 'react-icons/bi';
+import { FaSearch } from 'react-icons/fa';
+import { IoIosRestaurant } from 'react-icons/io';
+import { MdOutlineHotel } from 'react-icons/md';
 
 export default function Navbar() {
-
-	return (
-		<>
-			{/* <NavbarTop /> */}
-			<NavbarBottom />
-		</>
-	)
+    return (
+        <>
+            <NavbarBottom />
+        </>
+    );
 }
 
 const NavbarBottom = () => {
-
-	const NavItem = ({ label, link, icon }) => (
-		<Link href={link}>
-			<div className='flex flex-col items-center'>
-				<div>{icon}</div>
-				<div className='text-xs'>{label}</div>
-			</div>
-		</Link>
-	)
+    const NavItem = ({ label, link, icon }) => (
+        <Link href={link}>
+            <div className="flex flex-col items-center">
+                <div>{icon}</div>
+                <div className="text-xs">{label}</div>
+            </div>
+        </Link>
+    );
 
 	return (
 		<div className='p-2 sticky z-10 left-0 bottom-0 w-full md:hidden mt-auto'>
@@ -47,50 +43,50 @@ const NavbarBottom = () => {
 
 
 const useStyles = createStyles((theme) => ({
-	header: {
-		paddingLeft: theme.spacing.md,
-		paddingRight: theme.spacing.md,
-	},
+    header: {
+        paddingLeft: theme.spacing.md,
+        paddingRight: theme.spacing.md,
+    },
 
-	inner: {
-		height: rem(56),
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-	},
+    inner: {
+        height: rem(56),
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
 
-	links: {
-		[theme.fn.smallerThan('md')]: {
-			display: 'none',
-		},
-	},
+    links: {
+        [theme.fn.smallerThan('md')]: {
+            display: 'none',
+        },
+    },
 
-	search: {
-		[theme.fn.smallerThan('xs')]: {
-			display: 'none',
-		},
-	},
+    search: {
+        [theme.fn.smallerThan('xs')]: {
+            display: 'none',
+        },
+    },
 
-	link: {
-		display: 'block',
-		lineHeight: 1,
-		padding: `${rem(8)} ${rem(12)}`,
-		borderRadius: theme.radius.sm,
-		textDecoration: 'none',
-		color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-		fontSize: theme.fontSizes.sm,
-		fontWeight: 500,
+    link: {
+        display: 'block',
+        lineHeight: 1,
+        padding: `${rem(8)} ${rem(12)}`,
+        borderRadius: theme.radius.sm,
+        textDecoration: 'none',
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+        fontSize: theme.fontSizes.sm,
+        fontWeight: 500,
 
-		'&:hover': {
-			backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-		},
-	},
+        '&:hover': {
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        },
+    },
 }));
 
-const iconsSize = 20
+const iconsSize = 20;
 
 const links = [
-	{ label: 'Hoteluri', link: '/hotels', icon: <MdOutlineHotel size={iconsSize} /> },
+	{ label: 'Hotele', link: '/hotels', icon: <MdOutlineHotel size={iconsSize} /> },
 	{ label: 'Restaurante', link: '/restaurants', icon: <IoIosRestaurant size={iconsSize} /> },
 	{ label: 'Home', link: '/', icon: <AiOutlineHome size={iconsSize} /> },
 	{ label: 'Magazine', link: '/stores', icon: <BiStore size={iconsSize} /> },
@@ -98,42 +94,37 @@ const links = [
 ]
 
 const NavbarTop = () => {
-	const [opened, { toggle }] = useDisclosure(false);
-	const { classes } = useStyles();
+    const [opened, { toggle }] = useDisclosure(false);
+    const { classes } = useStyles();
 
-	const items = links.map((link) => (
-		<a
-			key={link.label}
-			href={link.link}
-			className={classes.link}
-			onClick={(event) => event.preventDefault()}
-		>
-			{link.label}
-		</a>
-	));
+    const items = links.map((link) => (
+        <a key={link.label} href={link.link} className={classes.link} onClick={(event) => event.preventDefault()}>
+            {link.label}
+        </a>
+    ));
 
-	return (
-		<Header height={56} className={classes.header}>
-			<div className={classes.inner}>
-				<Group>
-					<div className='text-center'>
-						<div className='text-xl leading-none'>CAHUL</div>
-						<div className='text-sm leading-none'>CONNECT</div>
-					</div>
-				</Group>
+    return (
+        <Header height={56} className={classes.header}>
+            <div className={classes.inner}>
+                <Group>
+                    <div className="text-center">
+                        <div className="text-xl leading-none">CAHUL</div>
+                        <div className="text-sm leading-none">CONNECT</div>
+                    </div>
+                </Group>
 
-				<Group>
-					<Group ml={50} spacing={5} className={classes.links}>
-						{items}
-					</Group>
-					<Autocomplete
-						className={classes.search}
-						placeholder="Cauta..."
-						icon={<FaSearch size="1rem" stroke={1.5} />}
-						data={['React', 'Angular', 'Vue', 'Next.js', 'Riot.js', 'Svelte', 'Blitz.js']}
-					/>
-				</Group>
-			</div>
-		</Header>
-	);
-}
+                <Group>
+                    <Group ml={50} spacing={5} className={classes.links}>
+                        {items}
+                    </Group>
+                    <Autocomplete
+                        className={classes.search}
+                        placeholder="Cauta..."
+                        icon={<FaSearch size="1rem" stroke={1.5} />}
+                        data={['React', 'Angular', 'Vue', 'Next.js', 'Riot.js', 'Svelte', 'Blitz.js']}
+                    />
+                </Group>
+            </div>
+        </Header>
+    );
+};
--- pages/transport/index.tsx
+++ pages/transport/index.tsx
@@ -1,13 +1,16 @@
 import { numberEightCoords, numberFiveCoords, numberFourCoords, numberOneCoords } from '@/apps/transport/route-coords';
 import TransportMap from '@/components/TransportMap';
-import { Select } from '@mantine/core';
+import { Button, Checkbox, SegmentedControl, Select, TextInput } from '@mantine/core';
 import 'ol/ol.css';
 import { useState } from 'react';
 
 const Transport = () => {
+    const [section, setSection] = useState('buses');
+    const [rideType, setRideType] = useState('cheap');
+    const [checked, setChecked] = useState(false);
     const [route, setRoute] = useState<string | null>('routeOne');
 
-    const test = [
+    const routes = [
         { value: 'routeOne', label: 'Ruta numarul 1', coords: numberOneCoords },
         { value: 'routeFour', label: 'Ruta numarul 4', coords: numberFourCoords },
         { value: 'routeFive', label: 'Ruta numarul 5', coords: numberFiveCoords },
@@ -15,28 +18,95 @@ const Transport = () => {
     ];
 
     return (
-        <>
-            <h1 className="title">Transport</h1>
+        <div className="flex flex-col">
+            <div>
+                <h1 className="title">Transport</h1>
 
-            <h2 className="title title_md mt-6">Autobuze</h2>
-            <Select className="mb-4" value={route} onChange={setRoute} label="Selectati ruta" data={test} />
-            <TransportMap coords={test.find((item) => item.value === route)?.coords!}></TransportMap>
-
-            <h2 className="title title_md mt-6">Taxi</h2>
-            <div className="text-center mb-2 font-bold">City Taxi Cahul</div>
-            <div className="flex justify-around">
-                <div className="flex flex-col">
-                    <a href="tel:+373079699317">+373 079 699 317</a>
-                    <a href="tel:+373079338253">+373 079 338 253</a>
-                </div>
-                <div className="flex flex-col">
-                    <a href="tel:+373078074234">+373 078 074 234</a>
-                    <a href="tel:+373069188385">+373 069 188 385</a>
-                </div>
+                <SegmentedControl
+                    value={section}
+                    onChange={setSection}
+                    className="my-3"
+                    fullWidth
+                    size="md"
+                    data={[
+                        { label: 'Autobuze', value: 'buses' },
+                        { label: 'Taxi', value: 'taxi' },
+                    ]}
+                />
             </div>
 
-            <div className="h-52"></div>
-        </>
+            {section === 'buses' ? (
+                <>
+                    <Select className="mb-4" value={route} onChange={setRoute} label="Selectati ruta" data={routes} />
+                    <TransportMap coords={routes.find((item) => item.value === route)?.coords!}></TransportMap>
+                </>
+            ) : (
+                <form className="flex flex-col justify-between gap-5">
+                    <div className="flex flex-col gap-1.5">
+                        <TextInput placeholder="Numele dvs" label="Nume" withAsterisk />
+                        <TextInput placeholder="Numarul dvs de telefon" label="Numarul de telefon" withAsterisk />
+
+                        {!checked && <TextInput placeholder="Introduceti strada curenta" label="Strada" withAsterisk />}
+                    </div>
+
+                    <Checkbox
+                        checked={checked}
+                        onChange={(event) => setChecked(event.currentTarget.checked)}
+                        label="Foloseste geologatica curenta"
+                    />
+
+                    <SegmentedControl
+                        value={rideType}
+                        onChange={setRideType}
+                        data={[
+                            {
+                                label: (
+                                    <>
+                                        <div>Econom</div>
+                                        <div>
+                                            <span className="text-[10px] text-gray-600">de la </span>
+                                            <span className="font-bold text-xs me-[2px]">40</span>
+                                            <span className="font-bold text-[9px]">MDL</span>
+                                        </div>
+                                    </>
+                                ),
+                                value: 'cheap',
+                            },
+                            {
+                                label: (
+                                    <>
+                                        <div>Comfort</div>
+                                        <div>
+                                            <span className="text-[10px] text-gray-600">de la </span>
+                                            <span className="font-bold text-xs me-[2px]">60</span>
+                                            <span className="font-bold text-[9px]">MDL</span>
+                                        </div>
+                                    </>
+                                ),
+                                value: 'comfort',
+                            },
+                            {
+                                label: (
+                                    <>
+                                        <div>Livrare</div>
+                                        <div>
+                                            <span className="text-[10px] text-gray-600">de la </span>
+                                            <span className="font-bold text-xs me-[2px]">40</span>
+                                            <span className="font-bold text-[9px]">MDL</span>
+                                        </div>
+                                    </>
+                                ),
+                                value: 'delivery',
+                            },
+                        ]}
+                    />
+
+                    <Button className="bg-black h-12" color="dark" size="md">
+                        Comanda
+                    </Button>
+                </form>
+            )}
+        </div>
     );
 };
 
