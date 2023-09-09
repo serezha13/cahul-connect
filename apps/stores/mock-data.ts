a6be1d07a0206e2d9377e865af35099cd99c4eda
Loghin Sergiu <loghin_s13@icloud.com>
2023-09-10 02:30:45 +0300

    Stores final

--- apps/stores/mock-data.ts
+++ apps/stores/mock-data.ts
@@ -4,24 +4,26 @@ export const stores = [
         img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Linella_Alaturi_Zi_de_Zi.png/800px-Linella_Alaturi_Zi_de_Zi.png',
         name: 'Linella',
         promo: 'Reduceri',
-        description: 'Linella este o rețea sută la sută națională cu cea mai mare extindere geografică la nivel național. Înregistrează peste 150 de magazine, are o echipă de peste 4500 de angajați și de 21 de ani promovează comerțul modern în Republica Moldova. Linella este magazinul local cu cea mai bună ofertă în produse de necesitate zilnică. Suntem mereu în preajma consumatorilor, oferim servicii calitative, prețuri corecte și atmosferă plăcută.',
-        link: 'https://linella.md/ro',
+        description: 'Linella este o rețea sută la sută națională cu cea mai mare extindere geografică la nivel național. Înregistrează peste 150 de magazine, are o echipă de peste 4500 de angajați și de 21 de ani promovează comerțul modern în Republica Moldova.',
+        link: 'linella.md',
         tel: '+373 (22) 89-09-30',
         address: 'Piața Independenței 3, Cahul 3909',
-        workingHours: '08:00 AM - 23:00 PM (in fiecare zi)',
-        __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11105.409169576922!2d28.1939586!3d45.9042659!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b65dd9fa93cc13%3A0x4aa96d5cebd20e8d!2sLinella!5e0!3m2!1sen!2s!4v1694278455690!5m2!1sen!2s" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+        workingHours: '08:00 AM - 23:00 PM',
+        opened: true,
+        __html: `<iframe class="mt-4 w-full aspect-square" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11105.409169576922!2d28.1939586!3d45.9042659!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b65dd9fa93cc13%3A0x4aa96d5cebd20e8d!2sLinella!5e0!3m2!1sen!2s!4v1694278455690!5m2!1sen!2s" style={{ border: 0 }} allowFullscreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>`
     },
     {
         id: 2,
         img: 'https://files.rabota.md/images/uploads/2021_08_10_16_09_03_logo_fb.jpg',
         name: 'Local',
         promo: 'Reduceri',
-        description: 'Noua rețea de supermarketuri Local Discounter face parte din grupul de magazine Tarol DD. Acesta este unul din cei mai mari comercianți cu amănuntul din Republica Moldova. Local Discounter are ca scop deservirea rapidă și comodă a consumatorilor de toate vârstele, oferind produse proaspete și de calitate la cel mai bun preț și cât mai aproape de casa lor.',
-        link: 'https://croco.md/ru/store/local-discounter',
+        description: 'Noua rețea de supermarketuri Local Discounter face parte din grupul de magazine Tarol DD. Acesta este unul din cei mai mari comercianți cu amănuntul din Republica Moldova.',
+        link: 'local.md',
         tel: '+373 620-57-555',
         address: 'str Stefan cel Mare si Sfint 25, Cahul',
-        workingHours: '08:00 AM - 22:00 PM'
-        
+        workingHours: '08:00 AM - 22:00 PM',
+        opened: true,
+        __html: `<iframe class="mt-4 w-full aspect-square" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22209.250952919305!2d28.158656374316404!3d45.908183400000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b65d82e0b4083b%3A0xac79a19756c4d336!2sLOCAL!5e0!3m2!1sen!2s!4v1694301302015!5m2!1sen!2s" style={{ border: 0 }} allowFullscreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>`
     },
     {
         id: 3,
@@ -29,32 +31,39 @@ export const stores = [
         name: 'Fourchette',
         promo: 'Reduceri',
         description: 'Fourchette este locul unde poti gasi produse locale, la preturi foarte atractive! Lucrăm doar cu ferme și gospodării verificate și garantăm calitatea și prospețimea produselor. Veți găsi întotdeauna carne, pește și fructe de mare proaspete de înaltă calitate.',
-        link: 'https://shoppingmalldova.md/en/shops/fourchette/',
+        link: 'fourchette.md',
         tel: '+373 603-00-537',
         address: 'Strada Ștefan cel Mare 20, Cahul 3909',
-        workingHours: '08:00 AM - 22:00 PM'
+        workingHours: '08:00 AM - 22:00 PM',
+        opened: true,
+        __html: `
+        <iframe  class="mt-4 w-full aspect-square" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11104.896829200476!2d28.1924944!3d45.906827!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b65d2b4f98a023%3A0xb47d7c8ecdfe11b6!2sFourchette!5e0!3m2!1sen!2s!4v1694301384282!5m2!1sen!2s" style={{ border: 0 }} allowFullscreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>`
     },
     {
         id: 4,
         img: 'https://t3.ftcdn.net/jpg/01/04/96/72/360_F_104967250_OCmMpiv1SpIZkzNkdmOUFIrqrLNEfwct.jpg',
         name: 'STRUGURAS',
         nameS: 'Struguras',
-        promo: 'Reduceri',
+        promo: null,
         description: 'Fourchette este locul unde poti gasi produse locale, la preturi foarte atractive! Lucrăm doar cu ferme și gospodării verificate și garantăm calitatea și prospețimea produselor. Veți găsi întotdeauna carne, pește și fructe de mare proaspete de înaltă calitate.',
         link: null,
         tel: '+373 780-17-174',
         address: 'R38, Cahul',
-        workingHours: '24/7'
+        workingHours: '24/7',
+        opened: true,
+        __html: `<iframe class="mt-4 w-full aspect-square" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11104.778238175493!2d28.1937672!3d45.9074198!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b65cea9644cb2d%3A0xe6d6615e05806d2a!2sStrugura%C8%99!5e0!3m2!1sen!2s!4v1694301398805!5m2!1sen!2s" style={{ border: 0 }} allowFullscreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>`
     },
     {
         id: 5,
         img: 'https://t4.ftcdn.net/jpg/05/67/01/95/360_F_567019501_ilL2umFyX4cfa1l8tKQcJwW47l8ZS5cp.jpg',
         name: 'Flamingo',
-        promo: 'Reduceri',
+        promo: null,
         description: 'Acesta se concentrează pe a satisface nevoile de modă ale comunității locale, punând accent pe calitate și stil. Este posibil să ofere și promoții sau reduceri periodice pentru a răsplăti clienții fideli. Flamingo poate fi destinația potrivită pentru cei care doresc să își completeze garderoba cu articole la modă sau să găsească cadouri speciale.',
         link: null,
         tel: '+373  691-91-339',
         address: 'Calea Republicii 17A/1, Cahul 3909',
-        workingHours: '24/7'
+        workingHours: '24/7',
+        __html: `<iframe class="mt-4 w-full aspect-square" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11105.66358110866!2d28.1952906!3d45.9029941!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b65c947e48cbdb%3A0x1082bd03c171022e!2sFlamingo!5e0!3m2!1sen!2s!4v1694301358659!5m2!1sen!2s" style={{ border: 0 }} allowFullscreen loading="lazy" referrePpolicy="no-referrer-when-downgrade"></iframe>`,
+        opened: true
     }
 ]
